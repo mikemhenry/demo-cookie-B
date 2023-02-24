c7c5d0696de4e2f3048b7267d7d297870a05618a
Mike Henry <11765982+mikemhenry@users.noreply.github.com>
2023-02-24 08:10:03 -0700

    fixed flake8 errors

--- my_test/__init__.py
+++ my_test/__init__.py
@@ -1,5 +1,5 @@
 """A test"""
 
 # Add imports here
-from ._version import __version__
-from .my_test import *
+from ._version import __version__  # noqa: F401
+from .my_test import *  # noqa: F403, F401
